ed2d26cbadeac841fbf39f434a6538e20c2ac1fc
Kendrick Gardner <kbgar24@gmail.com>
2017-08-27 15:22:31 -0500

    Managing React Component STate with Lifecycle methods

--- src/App.js
+++ src/App.js
@@ -15,20 +15,28 @@ class App extends React.Component {
   //guaranteed to make into DOM, only fired once, before rendering to DOM
   componentWillMount(){
     console.log('componentWillMount')
+    this.setState({m: 2})
   }
 
   //after rendering to DOM
   componentDidMount(){
     console.log('componentDidMount')
+    console.log(ReactDOM.findDOMNode(this))
+    this.inc = setInterval(this.update, 500)
   }
 
   componentWillUnmount(){
     console.log('componentWillUnmount')
+    clearInterval(this.inc)
   }
 
   render(){
     console.log('render');
-    return <button onClick={this.update}>{this.state.val}</button>
+    return (
+      <button onClick={this.update}>
+        {this.state.val * this.state.m}
+      </button>
+    )
   }
 }
 
