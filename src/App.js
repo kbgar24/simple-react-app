65c47c942350a60b667100b1ee09cc86d5ea637f
Kendrick Gardner <kbgar24@gmail.com>
2017-08-27 14:28:13 -0500

    Adding nested Data with props.children

--- src/App.js
+++ src/App.js
@@ -1,45 +1,22 @@
 import React from 'react';
-import PropTypes from 'prop-types'
+// import PropTypes from 'prop-types'
 
 //Stateless function (Cannot have state)
 // const App = () => <h1>Hello World & Beyond</h1> 
 
 //Class component (Has State)
 class App extends React.Component {
-  constructor(){
-    super();
-    this.state = {
-      txt: 'this is the state txt',
-      cat: 0
-    }
-  }
+ render () {
+  return <Button>I <Heart /> React</Button>
+ }
+}
 
-  update(e) {
-    this.setState({txt: e.target.value})
-  }
-
-  render(){
-    return (
-      <div>
-        <h1>{this.state.txt}</h1>
-        <Widget update={this.update.bind(this)}/>
-      </div>
-    )
+class Heart extends React.Component {
+  render() {
+    return <span>&hearts;</span>
   }
 }
 
-const Widget = (props) => {
-  return <input type="text"
-          onChange={props.update}/>
-}
-
-App.propTypes = {
-  txt: PropTypes.string,
-  cat: PropTypes.number.isRequired
-}
-
-App.defaultProps = {
-  txt: "this is the default txt"
-}
+const Button = (props) => <button>{props.children}</button>
 
 export default App
